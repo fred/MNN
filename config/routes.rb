ae381f64357758357e05a30b5ee7e5647f671add
Frederico Araujo <fred.the.master@gmail.com>
2011-12-16 14:08:50 +0700

    devise for normal users

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Publication::Application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
-  devise_for :users, ActiveAdmin::Devise.config
-  # mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+  devise_for :users
 
   # opinio_model
 
