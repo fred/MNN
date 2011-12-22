6f64240cb9df7e030ee45fa2e789de0cbd7c0e3a
Frederico Araujo <fred.the.master@gmail.com>
2011-12-22 15:29:41 +0700

    disabled cloudfront until bug is fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Publication::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
   # Using Cloudfront url for assets
-  config.action_controller.asset_host = "http://d4b3aux6dw2bz.cloudfront.net"
+  # config.action_controller.asset_host = "http://d4b3aux6dw2bz.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
