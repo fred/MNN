bf76adae130f6eac4883d9fd849f1f78ec174c25
Frederico Araujo <fred.the.master@gmail.com>
2012-01-17 20:02:01 +0700

    Redis to go

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,5 +1,8 @@
 require 'resque/server'
 
+uri = URI.parse(ENV["REDISTOGO_URL"])
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
 AUTH_PASSWORD = "welcomez" || ENV['AUTH']
 
 if AUTH_PASSWORD
