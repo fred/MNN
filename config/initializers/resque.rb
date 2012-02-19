1cc43c36a366619a34a18a4322f6e730aff650f1
Frederico Araujo <fred.the.master@gmail.com>
2012-02-19 22:39:01 +0700

    fixing redis url

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,14 +1,13 @@
 require 'resque/server'
 
-if Rails.env.production?
+if ENV["REDISTOGO_URL"]
   uri = URI.parse(ENV["REDISTOGO_URL"])
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-  AUTH_PASSWORD = "welcomez" || ENV['AUTH']
-  if AUTH_PASSWORD
-    Resque::Server.use Rack::Auth::Basic do |username, password|
-      password == AUTH_PASSWORD
-    end
-  end
 else
   Resque.redis = Redis.new(:host => "127.0.0.1", :port => 6379)
+end
+
+AUTH_PASSWORD = "welcomez" || ENV['AUTH']
+Resque::Server.use Rack::Auth::Basic do |username, password|
+  password == AUTH_PASSWORD
 end
