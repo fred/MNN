f824aba16548eadb4750bf6d41fc99ee4ed44763
Frederico Araujo <fred.the.master@gmail.com>
2012-03-02 17:12:16 +0700

    Improved caching

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,25 +41,25 @@ class ItemsController < ApplicationController
     
     respond_to do |format|
       format.html {
-        headers['Cache-Control'] = 'public, max-age=600' unless (current_admin_user or current_user) # 10 min cache
+        headers['Cache-Control'] = 'private, max-age=600, must-revalidate' unless (current_admin_user or current_user)
         headers['Last-Modified'] = @last_published.httpdate
       }
       format.json {
-        headers['Cache-Control'] = 'public, max-age=600' unless (current_admin_user or current_user) # 10 min cache
+        headers['Cache-Control'] = 'public, max-age=600' unless (current_admin_user or current_user) # 10 min
         headers['Last-Modified'] = @last_published.httpdate
         render json: @items 
       }
       format.atom {
-        headers['Cache-Control'] = 'public, max-age=3600' unless (current_admin_user or current_user) # 1 hour cache
+        headers['Cache-Control'] = 'public, max-age=3600' unless (current_admin_user or current_user) # 1 hour
         headers['Last-Modified'] = @last_published.httpdate
         render :partial => "/shared/items", :layout => false }
       format.rss {
-        headers['Cache-Control'] = 'public, max-age=3600' unless (current_admin_user or current_user) # 1 hour cache
+        headers['Cache-Control'] = 'public, max-age=3600' unless (current_admin_user or current_user) # 1 hour
         headers['Last-Modified'] = @last_published.httpdate
         render :partial => "/shared/items", :layout => false 
       }
       format.xml {
-        headers['Cache-Control'] = 'public, max-age=600' unless (current_admin_user or current_user) # 10 min cache
+        headers['Cache-Control'] = 'public, max-age=600' unless (current_admin_user or current_user) # 10 min
         headers['Last-Modified'] = @last_published.httpdate
         render @items
       }
@@ -85,8 +85,8 @@ class ItemsController < ApplicationController
     @related = @item.solr_similar
     
     headers['Cache-Control'] = 'private, max-age=300, must-revalidate' unless (current_admin_user or current_user)
-    # 5 minutes cache, because of comments.
     headers['Last-Modified'] = @item.updated_at.httpdate
+    
     respond_to do |format|
       format.html
       format.json { render json: @item }
