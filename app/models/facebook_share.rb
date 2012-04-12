95c052ec392c9cf7fa3463b4765ae358011d649e
Frederico Araujo <fred.the.master@gmail.com>
2012-04-12 21:14:26 +0700

    Facebook share testing

---
+++ app/models/facebook_share.rb
@@ -0,0 +1,15 @@
+class FacebookShare < Share
+  belongs_to :item
+  after_create :enqueue
+  
+  protected
+  
+  def enqueue
+    if Rails.env.production?
+      Resque.enqueue(FacebookQueue,self.id)
+    else
+      Rails.logger.info("*** Resque: Updating Facebook status: #{self.id}")
+    end
+  end
+  
+end
