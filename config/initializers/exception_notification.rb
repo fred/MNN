1b2326030d3bba777d72174411c0f186f8bff2f2
Frederico Araujo <fred.the.master@gmail.com>
2012-04-20 15:24:08 +0700

    Fixed double name on commit

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,7 +2,3 @@ Rails.application.config.middleware.use ExceptionNotifier,
   :email_prefix => "[MNN_Error] ",
   :sender_address => %{"notifier" <inbox@worldmathaba.net>},
   :exception_recipients => %w{inbox@worldmathaba.net worldmathaba@gmail.com}
-  
-class ExceptionNotifier < ActionMailer::Base
-  self.delivery_method = :sendmail
-end
