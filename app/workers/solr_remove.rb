131c93331d1e17893d2a16d8a99da8f9d57b244a
Frederico Araujo <fred.the.master@gmail.com>
2012-04-22 18:11:37 +0700

    logo, adfreesite logo, css

--- app/workers/solr_remove.rb
+++ app/workers/solr_remove.rb
@@ -1,7 +1,11 @@
+require 'resque-history'
+
 class SolrRemove
+  extend Resque::Plugins::History
   @queue = :solr_remove
 
   def self.perform(classname, id)
+    Rails.logger.info("  Resque: Removing from SOLR item: #{id}")
     classname.constantize.find(id).solr_remove_from_index
   end
 end
--- app/workers/solr_update.rb
+++ app/workers/solr_update.rb
@@ -1,7 +1,11 @@
+require 'resque-history'
+
 class SolrUpdate
+  extend Resque::Plugins::History
   @queue = :solr_update
 
   def self.perform(classname, id)
+    Rails.logger.info("  Resque: Indexing to SOLR item: #{id}")
     classname.constantize.find(id).solr_index
   end
 end
