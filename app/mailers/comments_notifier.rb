fee6d339fcfd3b35dd3cb94ea19b3423b0462f52
Frederico Araujo <fred.the.master@gmail.com>
2012-05-19 08:46:04 +0700

    Fixed comment notification

--- app/mailers/comments_notifier.rb
+++ app/mailers/comments_notifier.rb
@@ -1,21 +1,24 @@
 class CommentsNotifier < ActionMailer::Base
-  default from: "WorldMathaba <inbox@worldmathaba.net>"
+  self.delivery_method = :smtp
 
   def new_comment(comment_id)
     @comment = Comment.includes(:owner).find(comment_id)
     @url  = url_for(admin_comment_path(@comment, only_path: false, protocol: 'http'))
     @subscriptions = CommentSubscription.where("email is not NULL").all
     @emails_list = @subscriptions.map{|t| t.email}.join(",")
-
-    unless @subscriptions.empty?
-      mail(
-        sender:   "inbox@worldmathaba.net",
-        reply_to: "inbox@worldmathaba.net",
-        to:       "inbox@worldmathaba.net",
-        bcc:      @emails_list,
-        subject:  "[New Comment] by #{@comment.display_name}",
-        tag:      "comment-notification"
-      )
-    end
+    @@smtp_settings = {
+      domain:    "worldmathaba.com",
+      address:   "localhost",
+      port:      25
+     }
+    mail(
+      from:     "WorldMathaba <inbox@worldmathaba.net>",
+      sender:   "inbox@worldmathaba.net",
+      reply_to: "inbox@worldmathaba.net",
+      to:       "inbox@worldmathaba.net",
+      bcc:      @emails_list,
+      subject:  "[New Comment] by #{@comment.display_name}",
+      tag:      "comment-notification"
+    )
   end
 end
--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,21 +1,18 @@
 class SubscriptionMailer < ActionMailer::Base
-  default from: "WorldMathaba <inbox@worldmathaba.net>"
   
   def new_item_email(item_id)
     @item = Item.find(item_id)
     @url  = url_for(item_path(@item, ref: "email", only_path: false, protocol: 'http'))
     @subscriptions = Subscription.where("email is not NULL AND item_id is NULL").all
     @emails_list = @subscriptions.map{|t| t.email}.join(",")
-    
-    unless @subscriptions.empty?
-      mail(
-        sender:   "inbox@worldmathaba.net",
-        reply_to: "inbox@worldmathaba.net",
-        to:       "inbox@worldmathaba.net",
-        bcc:      @emails_list,
-        subject:  "[New post] #{@item.title}",
-        tag:      "item_subscription"
-      )
-    end
+    mail(
+      from:     "WorldMathaba <inbox@worldmathaba.net>",
+      sender:   "inbox@worldmathaba.net",
+      reply_to: "inbox@worldmathaba.net",
+      to:       "inbox@worldmathaba.net",
+      bcc:      @emails_list,
+      subject:  "[New post] #{@item.title}",
+      tag:      "item_subscription"
+    )
   end
 end
