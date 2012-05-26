3f5a52eaff2874332511f0f028ffb820230ae7bd
Frederico Araujo <fred.the.master@gmail.com>
2012-05-26 15:43:48 +0700

    changed from Top Authors to Top Users on sidebar

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -8,7 +8,7 @@ module AuthorsHelper
         title: user.title
     )
     str += "<span class='item-title'>#{link_to user.title, author_path(user), title: user.title}</span>"
-    str += "<span class='item-date'>Published #{user.items_count} Articles</span>"
+    str += "<span class='item-date'>Submitted #{user.items_count} Articles</span>"
     str
   end
 
