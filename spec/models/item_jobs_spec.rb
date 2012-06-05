0f50fbdef3e40e8dc763c23287a98973af9de8de
Frederico Araujo <fred.the.master@gmail.com>
2012-06-05 08:02:05 +0700

    updated sidekiq

--- spec/models/item_jobs_spec.rb
+++ spec/models/item_jobs_spec.rb
@@ -4,22 +4,16 @@ describe Item do
   include NumericMatchers
 
   describe "with Job Queues" do
-    # require 'sidekiq/testing'
-    # it "should enqueue the email subscription job" do
-    #   expect {
-    #     FactoryGirl.create(:item, draft: false, published_at: Time.now, send_emails: "1")
-    #   }.to change(SubscriptionQueue.jobs, :size).by(1)
-    # end
-    # it "should enqueue the Twitter posting job" do
-    #   expect {
-    #     FactoryGirl.create(:item, draft: false, published_at: Time.now, share_twitter: "1")
-    #   }.to change(TwitterQueue.jobs, :size).by(1)
-    # end
-    # it "should enqueue the Twitter posting job" do
-    #   expect {
-    #     @item = FactoryGirl.create(:item, draft: false, published_at: Time.now, share_twitter: "1")
-    #     @item.comments << Comment.new(body: "testing")
-    #   }.to change(TwitterQueue.jobs, :size).by(2)
-    # end
+      require 'sidekiq/testing'
+    it "should enqueue the email subscription job" do
+      expect {
+        FactoryGirl.create(:item, draft: false, published_at: Time.now, send_emails: "1")
+      }.to change(SubscriptionQueue.jobs, :size).by(1)
+    end
+    it "should enqueue the email subscription job in 600 seconds" do
+      expect {
+        FactoryGirl.create(:item, draft: false, published_at: Time.now+600, send_emails: "1")
+      }.to change(SubscriptionQueue.jobs, :size).by(1)
+    end
   end
 end
