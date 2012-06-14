6799ab27b7d4712ccffa6237bec3d8f4400eb8bc
Frederico Araujo <fred.the.master@gmail.com>
2012-06-14 13:25:43 +0700

    blockquote font, updated bootstrap

--- app/admin/item.rb
+++ app/admin/item.rb
@@ -79,9 +79,14 @@ ActiveAdmin.register Item do
     end
     column "Live", sortable: :published_at do |item|
       if !item.draft? && item.published_at && item.published_at > Time.zone.now
-        ("<span class='red'>" +
+        ("<span class='red bold'>" +
         distance_of_time_in_words(item.published_at, Time.zone.now) +
         " from now" +
+        '</span>').html_safe
+      elsif !item.draft? && item.published_at
+        ("<span class='green bold'>" +
+        distance_of_time_in_words(item.published_at, Time.zone.now) +
+        " ago" +
         '</span>').html_safe
       else
         item.published_at.localtime.to_s(:short)
