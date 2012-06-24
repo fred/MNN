2ffdb91169d1c0e7dfc7385e48d6be91d7251924
Frederico Araujo <fred.the.master@gmail.com>
2012-06-24 14:29:59 +0700

    reduced lifetime of assets

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -15,25 +15,15 @@ if Rails.env.test? or Rails.env.cucumber?
 else
   CarrierWave.configure do |config|
     config.cache_dir = "#{Rails.root}/tmp/uploads"
-
-    # config.cache_dir = 'carrierwave' # ...two lines
-    # config.root = Rails.root.join('tmp') # adding these...
-  
     config.fog_credentials = {
       :provider               => 'AWS',               # required
       :aws_access_key_id      => ENV["S3_KEY"],       # required
       :aws_secret_access_key  => ENV["S3_SECRET"],    # required
-      :region                 => ENV['S3_REGION']  # optional, defaults to 'us-east-1'
+      :region                 => ENV['S3_REGION']     # optional, defaults to 'us-east-1'
     }
-    config.fog_directory  =  ENV['S3_BUCKET']                       # required
-    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+    config.fog_directory  =  ENV['S3_BUCKET']         # required
+    config.fog_attributes = {'Cache-Control'=>'max-age=604800'}  # 1 week, optional, defaults to {}
   end
 end
-# if Rails.env.production?
-#   config.storage = :fog
-# else
-#   config.storage = :file
-# end
-
 
 Excon.defaults[:ssl_verify_peer] = false
