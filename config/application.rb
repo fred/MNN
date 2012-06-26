1198d1cb6272e91fda970b607ee85851275b49f9
Frederico Araujo <fred.the.master@gmail.com>
2012-06-27 02:11:17 +0700

    Refresh and refactor assets

--- config/application.rb
+++ config/application.rb
@@ -67,7 +67,7 @@ module Publication
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.0.1'
     
     # Heroku
     config.assets.initialize_on_precompile = false
