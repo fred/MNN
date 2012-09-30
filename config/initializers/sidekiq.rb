c0adbe376ceb5703cb46f6b31a6740266458a24a
Frederico Araujo <fred.the.master@gmail.com>
2012-09-30 15:38:47 +0700

    Added Namespace to sidekiq based on path and env

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,16 @@
+namespace = OpenSSL::HMAC.hexdigest('md5', Dir.pwd, Rails.env)[0..15]
+
+# By default, sidekiq assumes Redis is at localhost:6379. 
+# This is fine for development but for many deployments you'll need to point sidekiq to an external 
+# Redis server and an optional namespace by throwing this in config/initializers/sidekiq.rb
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://127.0.0.1:6379', namespace: namespace }
+end
+# Next, you need to configure the Sidekiq client, which is similar.
+# If you're using the client with a single-threaded Rails (or other ruby) process,
+# add a size of 1, which will provide one Redis connection for the client:
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://127.0.0.1:6379', namespace: namespace }
+end
