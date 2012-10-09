1ff8f7837cc63f4f35bdcf86abbcfe2af8513122
Frederico Araujo <fred.the.master@gmail.com>
2012-10-09 18:57:08 +0700

    added indexes to feed migrations

--- db/migrate/20120909081114_create_feed_sites.rb
+++ db/migrate/20120909081114_create_feed_sites.rb
@@ -7,11 +7,14 @@ class CreateFeedSites < ActiveRecord::Migration
       t.text     :description
       t.integer  :category_id
       t.integer  :feed_type
+      t.integer  :user_id
       t.datetime :last_modified
       t.string   :site_url
       t.string   :logo_url
       t.integer  :sort_order,  default: 100
       t.timestamps
     end
+    add_index :feed_sites, :user_id
+    add_index :feed_sites, :category_id
   end
 end
--- db/migrate/20120909081145_create_feed_entries.rb
+++ db/migrate/20120909081145_create_feed_entries.rb
@@ -4,11 +4,13 @@ class CreateFeedEntries < ActiveRecord::Migration
       t.string   :title
       t.string   :url
       t.string   :author
+      t.string   :image
       t.text     :summary
       t.text     :content
       t.datetime :published
       t.integer  :feed_site_id
       t.timestamps
     end
+    add_index :feed_entries, :feed_site_id
   end
 end
