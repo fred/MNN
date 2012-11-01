e7c7572a2c35e0a8ca45e7e6c0745446bc7a49e1
Frederico Araujo <fred.the.master@gmail.com>
2012-11-01 16:26:24 +0700

    Member only pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,21 +1,22 @@
 class Page < ActiveRecord::Base
-  
   validates_presence_of :title, :body
-  
   belongs_to :language
   belongs_to :user
-  
+
   attr_accessor :updated_reason
-  
+
   # Versioning System
   has_paper_trail meta: { tag: :updated_reason }
-  
+
   # Permalink URLS
   extend FriendlyId
   friendly_id :title, use: :slugged
-  
+
   def self.active
     where(active: true)
   end
-  
+
+  def self.member_only
+    where(member_only: true)
+  end
 end
