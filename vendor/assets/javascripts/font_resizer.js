5b0cf638bf74ca066e698c0b743370c15e8ec739
Frederico Araujo <fred.the.master@gmail.com>
2012-11-12 08:29:24 +0700

    font-resizer refactor, css and assets

--- vendor/assets/javascripts/font_resizer.js
+++ vendor/assets/javascripts/font_resizer.js
@@ -6,40 +6,31 @@ $(document).ready(function() {
     var min=9;
     var normal=14;
     var max=20;
+    
     $("#resetFont").click(function() {
-       var p = document.getElementsByTagName('p');
-       for(i=0;i<p.length;i++) {
-          p[i].style.fontSize = normal+"px";
-       }
+      $('#item_body p').each(function() {
+        $(this).css("font-size", normal+"px");
+      });
     });
+
     $("#increaseFont").click(function() {
-       var p = document.getElementsByTagName('p');
-       for(i=0;i<p.length;i++) {
-          if(p[i].style.fontSize) {
-             var s = parseInt(p[i].style.fontSize.replace("px",""));
-          } else {
-             var s = normal;
-          }
-          if(s!=max) {
-             s += 1;
-          }
-          p[i].style.fontSize = s+"px";
-       }
+      $('#item_body p').each(function() {
+        p = this;
+        s = parseInt($(p).css("font-size").replace("px",""));
+        s += 1;
+        $(p).css("font-size", s+"px");
+      });
     });
+
     $("#decreaseFont").click(function() {
-       var p = document.getElementsByTagName('p');
-       for(i=0;i<p.length;i++) {
-          if(p[i].style.fontSize) {
-             var s = parseInt(p[i].style.fontSize.replace("px",""));
-          } else {
-             var s = normal;
-          }
-          if(s!=min) {
-             s -= 1;
-          }
-          p[i].style.fontSize = s+"px";
-       }
+      $('#item_body p').each(function() {
+        p = this;
+        s = parseInt($(p).css("font-size").replace("px",""));
+        s -= 1;
+        $(p).css("font-size", s+"px");
+      });
     });
+
     // Show the font-resizer for non-touch devices
     $("#font_control").show();
   }
