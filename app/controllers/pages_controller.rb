896cb3df1b2b725aafe30cc9b1f973d9319f304a
Frederico Araujo <fred.the.master@gmail.com>
2013-02-01 18:39:52 +0700

    updated gems

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,4 +15,12 @@ class PagesController < InheritedResources::Base
     @pages = Page.active
   end
 
+  def edit
+    redirect_to root_path
+  end
+
+  def new
+    redirect_to root_path
+  end
+
 end
