70a89b2667584c2e6644fd1cdefd14de151ba705
Frederico Araujo <fred.the.master@gmail.com>
2013-02-05 17:43:08 +0700

    reduced box size, Firefox font fix

--- db/migrate/20130114061835_create_hstore_migration.rb
+++ db/migrate/20130114061835_create_hstore_migration.rb
@@ -1,9 +1,11 @@
 class CreateHstoreMigration < ActiveRecord::Migration
   def self.up
-    create_extension "hstore"
+    # create_extension "hstore"
+    execute "CREATE EXTENSION IF NOT EXISTS hstore"
   end
 
   def self.down
-    drop_extension "hstore"
+    # drop_extension "hstore"
+    execute "DROP EXTENSION IF EXISTS hstore"
   end
 end
