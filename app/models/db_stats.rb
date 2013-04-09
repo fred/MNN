06cc92772cd832bf108fba8e8b3adecdb7d23f6f
Frederico Araujo <fred.the.master@gmail.com>
2013-04-09 22:57:11 +0700

    fixed divide by zero in postgresql stats SQL

--- app/models/db_stats.rb
+++ app/models/db_stats.rb
@@ -17,7 +17,7 @@ class DbStats
   def self.index_status(lim=20)
     sql = "
     SELECT relname,
-      100 * idx_scan / (seq_scan + idx_scan) percent_of_times_index_used, 
+      100 * idx_scan / (seq_scan + greatest(1,idx_scan)) percent_of_times_index_used,
       n_live_tup rows_in_table
     FROM pg_stat_user_tables 
     ORDER BY n_live_tup DESC
